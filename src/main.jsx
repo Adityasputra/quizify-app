52ecf4f0ebf39383877f0d9d9cb990e1f1389ec4
Aditya Saputra <aditsputra.cv@gmail.com>
2026-02-06 10:52:24 +0700

    feat: wrap RouterProvider with QuizProvider to manage quiz state

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
 import router from "./routes.jsx";
+import { QuizProvider } from "./context/QuizContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QuizProvider>
+      <RouterProvider router={router} />
+    </QuizProvider>
   </StrictMode>,
 );
