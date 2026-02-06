705a8c925a04a5a101773b85dcd79db520f0395e
Aditya Saputra <aditsputra.cv@gmail.com>
2026-02-06 10:02:26 +0700

    feat: add navigation button to login in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,12 @@
+import { useNavigate } from "react-router";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello, World!</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+      <button onClick={() => navigate("/login")}>Go to Login</button>
+    </>
+  );
 }
