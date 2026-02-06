62201345afa2b26319802d7a4995c76ac3ce7d3a
Aditya Saputra <aditsputra.cv@gmail.com>
2026-02-06 10:52:39 +0700

    feat: add routes for Quiz and Result components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz />,
+  },
+  {
+    path: "/result",
+    element: <Result />,
   },
 ]);
 
